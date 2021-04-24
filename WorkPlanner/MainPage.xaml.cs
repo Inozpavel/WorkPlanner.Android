daa26c86bfa124eb6e72cb3dc54dfd2a3ba2dc95
Inozpavel <inozpavel@mail.ru>
2021-04-24 17:54:39 +0300

    Initial commit

---
+++ WorkPlanner/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WorkPlanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
